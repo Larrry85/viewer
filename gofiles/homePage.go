cbb6922a00d21381397890515eff69029b20c793
lauralevisto <levisto.laura@gmail.com>
2024-06-07 11:18:12 +0300

    homePage comments

--- gofiles/homePage.go
+++ gofiles/homePage.go
@@ -1,45 +1,44 @@
 package gofiles
 
 import (
-	"encoding/json"
-	"fmt"
-	"html/template"
-	"net/http"
-	"os"
-	"path/filepath"
+	"encoding/json"		// decoding JSON data
+	"fmt"				// formatting strings
+	"html/template"		// rendering HTML templates
+	"net/http"			// handling HTTP requests and responses
+	"os"				// file operations
+	"path/filepath"		// manipulating file paths
 )
 
 // data.json path
 var directory = "api"
 var fileName = "data.json"
-var filePath = filepath.Join(directory, fileName)
+var filePath = filepath.Join(directory, fileName) // full path to the JSON file
 
-// HomePage reads data from data.json, parses it into CarsData,
-// and renders it using index.html
+// HTTP handler that will serve the homepage
 func HomePage(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.Error(w, "404 not found.", http.StatusNotFound)
+	if r.URL.Path != "/" { // checks if the URL path is "/"
+		http.Error(w, "404 not found.", http.StatusNotFound) // 404
 		return
 	}
 
-	// Reads and parses JSON data from data.json
+	// reads the content of the JSON file
 	data, err := os.ReadFile(filePath)
 	if err != nil {
-		http.Error(w, fmt.Sprintf("Failed to read file %s: %v", filePath, err), http.StatusInternalServerError)
+		http.Error(w, fmt.Sprintf("Failed to read file %s: %v", filePath, err), http.StatusInternalServerError) // 500
 		return
 	}
 
-	// Parse json data into variables
+	// parses the JSON data into a CarsData struct
 	var carData CarsData
 	if err := json.Unmarshal(data, &carData); err != nil {
-		http.Error(w, fmt.Sprintf("Failed to parse JSON: %v", err), http.StatusInternalServerError)
+		http.Error(w, fmt.Sprintf("Failed to parse JSON: %v", err), http.StatusInternalServerError) // 500
 		return
 	}
 
-	// Extract unique years
+	// calls the getUniqueYears function to extract unique years from the car models
 	uniqueYears := getUniqueYears(carData.CarModels)
 
-	// Combine car data and unique years into a single struct
+	// combines CarsData and unique years into a struct to pass to the template
 	dataWithYears := struct {
 		CarsData
 		Years []int
@@ -48,14 +47,14 @@ func HomePage(w http.ResponseWriter, r *http.Request) {
 		Years:    uniqueYears,
 	}
 
-	// Render the HTML template with the car data
+	// parses the index.html template
 	tmpl, err := template.ParseFiles("static/index.html")
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		http.Error(w, err.Error(), http.StatusInternalServerError) // 500
 		return
-	}
+	} // executes the template with the combined data (dataWithYears)
 	if err := tmpl.Execute(w, dataWithYears); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		http.Error(w, err.Error(), http.StatusInternalServerError) // 500
 		return
 	}
 }
